3b381108d8470cea163ddf9e4765c65e5c67a7d5
Gi <00001114792998sp@al.educacao.sp.gov.br>
2024-10-29 15:00:40 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Biologia',
+    'Qual é a função principal das mitocôndrias nas células?',
+    'A função principal das mitocôndrias é produzir energia para a célula, através de um processo chamado respiração celular.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é fotossíntese e qual o seu papel para as plantas?',
+    'Fotossíntese é o processo pelo qual as plantas produzem seu próprio alimento usando a luz solar, água e dióxido de carbono, liberando oxigênio como subproduto.'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o símbolo químico da água?',
+    'O símbolo químico da água é H2O'
+)
+
+criaCartao(
+    'Química',
+    'Qual gás é essencial para a combustão e muito presente no ar que respiramos?',
+    'O gás essencial para a combustão é o oxigênio 02'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo?',
+    'Ásia'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o rio mais longo da América do Sul?',
+    'O rio mais longo da América do Sul é o rio Amazonas.'
+)
